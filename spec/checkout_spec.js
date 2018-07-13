2994254946feb49cb0b02fcc2a77ad21d4907680
Elishka Flint <elishkaf@gmail.com>
2018-07-13 10:19:50 +0100

    FEATURE Add .scan function to satisfy second user story and pass tests

--- spec/checkout_spec.js
+++ spec/checkout_spec.js
@@ -3,6 +3,7 @@ describe("checkout", function() {
   describe(".scan", function() {
     it('confirms that an item has been scanned', function() {
       var item = jasmine.createSpy('item');
+      var checkout = new Checkout();
       expect(checkout.scan(item)).toEqual('Item scanned');
     });
   });
---
+++ src/checkout.js
@@ -0,0 +1,6 @@
+function Checkout() {
+}
+
+Checkout.prototype.scan = function(item) {
+  return 'Item scanned';
+};
