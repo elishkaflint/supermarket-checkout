bce2bf678475b4e43c7983dfdd5b07379b3534bd
Elishka Flint <elishkaf@gmail.com>
2018-07-13 11:31:36 +0100

    REFACTOR feature tests

--- spec/features_spec.js
+++ spec/features_spec.js
@@ -1,13 +1,22 @@
 describe("supermarket checkout", function() {
 
+  var item1;
+  var item2;
+  var checkout;
+
+  beforeEach(function() {
+    checkout = new Checkout();
+    item1 = new Item('item1', 1);
+    item2 = new Item('item2', 2);
+  });
+
   // As a shopper
   // So I know how much an item costs
   // I would like to be able to see its price
 
   describe("item cost", function() {
     it('the user can see the the price of an item', function() {
-      var item = new Item('bread', 1);
-      expect(item.getPrice()).toEqual(1);
+      expect(item1.getPrice()).toEqual(1);
     });
   });
 
@@ -17,15 +26,11 @@ describe("supermarket checkout", function() {
 
   describe("scanning an item", function() {
     it('the user can scan an item at the checkout', function() {
-      var item = new Item('bread', 1);
-      var checkout = new Checkout();
-      expect(checkout.scan(item)).toEqual('Item scanned');
+      expect(checkout.scan(item1)).toEqual('Item scanned');
     });
     it('the checkout tracks the scanned items', function() {
-      var bread = new Item('bread', 1);
-      var checkout = new Checkout();
-      checkout.scan(bread);
-      expect(checkout.items).toContain(bread);
+      checkout.scan(item1);
+      expect(checkout.items).toContain(item1);
     });
   });
 
@@ -39,21 +44,10 @@ describe("supermarket checkout", function() {
 
   describe("calculating the total", function() {
     it('the user can see the total of scanned items in the correct format', function() {
-      var bread = new Item('bread', 1);
-      var milk = new Item('milk', 2);
-      var checkout = new Checkout();
-      checkout.scan(bread);
-      checkout.scan(milk);
+      checkout.scan(item1);
+      checkout.scan(item2);
       expect(checkout.total()).toEqual('£3.00');
     });
   });
 
-
-
 });
-
-
-
-// As a shopper
-// So that I know how much to pay
-// I would like to see all prices correctly formatted (£xx.xx)
